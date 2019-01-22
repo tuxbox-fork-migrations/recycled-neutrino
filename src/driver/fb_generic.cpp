tuxbox-fork-migrations/recycled-neutrino

commit a4a7a4b32cbef26fd7b1ff8fbf3b56559a53ba7c

screensaver.cpp: use fallback if video decoder is busy
Origin commit data
------------------
Branch: master
Commit: https://github.com/tuxbox-neutrino/neutrino/commit/8b7de5fc8f045ca5170f6e006e5dc836a2db6bee
Author: GetAway <get-away@t-online.de>
Date: 2019-01-13 (Sun, 13 Jan 2019)


------------------
No further description and justification available within origin commit message!

------------------
This commit was generated by Migit

src/driver/fb_generic.cpp
@@ -38,6 +38,7 @@
 #include <signal.h>
 #include <sys/ioctl.h>
 #include <unistd.h>
+#include <errno.h>
 #include <sys/mman.h>
 #include <memory.h>
 #include <math.h>
@@ -50,6 +51,7 @@
 #include <gui/osd_helpers.h>
 #include <gui/pictureviewer.h>
 #include <system/debug.h>
+#include <zapit/settings.h>
 #include <global.h>
 #include <hardware/video.h>
 #include <cs_api.h>
@@ -1546,15 +1548,24 @@ void CFrameBuffer::Clear()
 	//memset(getFrameBufferPointer(), 0, stride * yRes);
 }
 
-bool CFrameBuffer::showFrame(const std::string & filename)
+bool CFrameBuffer::showFrame(const std::string & filename, bool fallback)
 {
 	std::string picture = getIconPath(filename, "");
-	if (access(picture.c_str(), F_OK) == 0){
-		videoDecoder->ShowPicture(picture.c_str());
-		return true;
+	if (access(picture.c_str(), F_OK) == 0)
+	{
+		if (videoDecoder->ShowPicture(picture.c_str()))
+			return true;
+		else
+		{
+			if (fallback) {
+				return g_PicViewer->DisplayImage(picture, 0, 0, getScreenWidth(true), getScreenHeight(true), TM_NONE);
+			}
+			else
+				dprintf(DEBUG_NORMAL,"[CFrameBuffer]\[%s - %d], fallback is disabled, paint of image was stopped: %s\n", __func__, __LINE__, picture.c_str());
+		}
 	}
 	else
-		printf("[CFrameBuffer]\[%s - %d], image not found: %s\n", __func__, __LINE__, picture.c_str());
+		dprintf(DEBUG_NORMAL,"[CFrameBuffer]\[%s - %d], image not found: %s\n", __func__, __LINE__, picture.c_str());
 
 	return false;
 }

src/gui/screensaver.cpp
@@ -302,7 +302,7 @@ void CScreenSaver::paint()
 
 		dprintf(DEBUG_INFO, "[CScreenSaver]  %s - %d : %s\n",  __func__, __LINE__, v_bg_files.at(index).c_str());
 #if HAVE_ARM_HARDWARE
-		m_frameBuffer->showFrame(v_bg_files.at(index));
+		m_frameBuffer->showFrame(v_bg_files.at(index), true);
 #else
 		paintImage(v_bg_files.at(index), 0, 0, m_frameBuffer->getScreenWidth(true), m_frameBuffer->getScreenHeight(true));
 #endif
@@ -322,7 +322,7 @@ void CScreenSaver::paint()
 			scr_clock->setClockFont(g_Font[SNeutrinoSettings::FONT_TYPE_INFOBAR_NUMBER]);
 			scr_clock->disableSaveBg();
 			scr_clock->doPaintBg(false);
-			m_frameBuffer->showFrame("blackscreen.jpg");
+			m_frameBuffer->showFrame("blackscreen.jpg", true);
 		}
 
 		scr_clock->setTextColor(clr.i_color);

src/driver/fb_generic.h
@@ -266,7 +266,7 @@ class CFrameBuffer : public sigc::trackable
 		void RestoreScreen(int x, int y, int dx, int dy, fb_pixel_t * const memp);
 
 		void Clear();
-		bool showFrame(const std::string & filename);
+		bool showFrame(const std::string & filename, bool fallback = false);
 		void stopFrame();
 		bool loadBackgroundPic(const std::string & filename, bool show = true);
 		bool Lock(void);

commit 15c9a0fd9b27dd54ba1d4a3d6d1c6a7633e10f6e

fb_generic.cpp: fix build for CST hardware...
was broken with commit a4a7a4b


Origin commit data
------------------
Branch: master
Commit: https://github.com/tuxbox-neutrino/neutrino/commit/c14b34fe54c8e71024c11a79b7686f03e15c4250
Author: GetAway <get-away@t-online.de>
Date: 2019-01-22 (Tue, 22 Jan 2019)



------------------
This commit was generated by Migit

src/driver/fb_generic.cpp
@@ -1549,6 +1549,14 @@ void CFrameBuffer::Clear()
 bool CFrameBuffer::showFrame(const std::string & filename, bool fallback)
 {
 	std::string picture = getIconPath(filename, "");
+#if HAVE_COOL_HARDWARE
+	(void)fallback;
+	if (access(picture.c_str(), F_OK) == 0)
+	{
+		videoDecoder->ShowPicture(picture.c_str());
+		return true;
+	}
+#else
 	if (access(picture.c_str(), F_OK) == 0)
 	{
 		if (videoDecoder->ShowPicture(picture.c_str()))
@@ -1562,6 +1570,7 @@ bool CFrameBuffer::showFrame(const std::string & filename, bool fallback)
 				dprintf(DEBUG_NORMAL,"[CFrameBuffer]\[%s - %d], fallback is disabled, paint of image was stopped: %s\n", __func__, __LINE__, picture.c_str());
 		}
 	}
+#endif
 	else
 		dprintf(DEBUG_NORMAL,"[CFrameBuffer]\[%s - %d], image not found: %s\n", __func__, __LINE__, picture.c_str());
 

src/gui/screensaver.cpp
@@ -301,11 +301,7 @@ void CScreenSaver::paint()
 		}
 
 		dprintf(DEBUG_INFO, "[CScreenSaver]  %s - %d : %s\n",  __func__, __LINE__, v_bg_files.at(index).c_str());
-#if HAVE_ARM_HARDWARE
 		m_frameBuffer->showFrame(v_bg_files.at(index), true);
-#else
-		paintImage(v_bg_files.at(index), 0, 0, m_frameBuffer->getScreenWidth(true), m_frameBuffer->getScreenHeight(true));
-#endif
 
 		if (!g_settings.screensaver_random)
 			index++;